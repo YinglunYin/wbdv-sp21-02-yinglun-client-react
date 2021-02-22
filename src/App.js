f5341a1b145a088e7809cf1c58db913589a5afec
Yinglun Yin <adsl7377@163.com>
2021-02-22 15:08:35 +0800

    Assignment 3 Finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import CourseManager from "./components/course-manager";
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from './components/home'
 
 function App() {
     return (
         // eslint-disable-next-line react/jsx-no-undef
         <BrowserRouter>
             <div>
+                <Route path='/courses'>
                 <CourseManager/>
+                </Route>
+                <Route path="/" exact={true}>
+                    <Home/>
+                </Route>
             </div>
         </BrowserRouter>
 
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+const Home = () =>
+    <div>
+        <h1>Course Manager</h1>
+        <Link to={"/courses/table"}><h2>Course Table</h2></Link>
+        <Link to={"/courses/grid"}><h2>Course Grid</h2></Link>
+        <Link to={"/courses/editor"}><h2>Course Editor</h2></Link>
+    </div>
+
+export default Home
